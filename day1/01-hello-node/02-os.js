00fbd84a6a95961d9956fd5f6666f70eb149f1c8
Michael Meisel <mrmeisel@gmail.com>
2022-10-23 11:16:37 -0700

    Use os module instead of process as second example

---
+++ day1/01-hello-node/02-os.js
@@ -0,0 +1,5 @@
+const os = require('os');
+
+console.log('Operating system version:', os.version());
+console.log('Uptime:', os.uptime() / 3600, 'hours');
+console.log('User info:', os.userInfo());
--- day1/01-hello-node/02-process.js
+++
@@ -1,3 +0,0 @@
-console.log("current working directory: " + process.cwd());
-console.log("running on platform: " + process.platform);
-console.log("running on architecture: " + process.arch);
