d4da2af010b0f2d4694ccf7e371e3f3ddb82a3a1
Michael Meisel <mrmeisel@gmail.com>
2022-10-26 23:58:23 -0700

    Better johnny-five examples

--- day2/07-j5-repl/index.js
+++ day2/07-j5-repl/index.js
@@ -5,7 +5,7 @@ const board = new five.Board();
 
 board.on('ready', () => {
   // Use 13 for the built-in LED, 11 is to match the previous example
-  const led = new five.Led(process.argv[2] || 11);
+  const led = new five.Led(11);
 
   board.repl.inject({
     // Allow limited on/off control access to the Led instance from the command line.
